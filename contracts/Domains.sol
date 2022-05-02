d52817265b112fdba0038955188ec657cc5fc2a4
Suresh <sureshcse@gmail.com>
2022-05-02 20:15:08 +0530

    initial commit

---
+++ contracts/Domains.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+
+contract Domains{
+
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
+
+    constructor(){
+        console.log("This is my domain name contract");
+    }
+
+    function register(string calldata name) public{
+        require(domains[name] == address(0));
+        domains[name]=msg.sender;
+        console.log("%s has registered a domain",msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns(address){
+        return domains[name];
+    }
+    function setRecord(string calldata name, string calldata record) public {
+        // Check that the owner is the transaction sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
+    }
+}
+
